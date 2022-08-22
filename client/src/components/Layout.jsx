57ca918d9a74150ede1504246f477935761bac47
animamundi166 <animamundi166@gmail.com>
2022-08-22 19:58:59 +0300

    add get id posts

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,11 +2,11 @@ import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
   return (
-      <div className='container mx-auto'>
-        <Navbar />
-        {children}
-      </div>
-  )
-}
+    <div className='container mx-auto'>
+      <Navbar />
+      {children}
+    </div>
+  );
+};
 
 export default Layout;
